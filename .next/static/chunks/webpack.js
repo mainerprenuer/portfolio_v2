6cf4da402a4a9fe26fcaf514e0c867b4fa09356d
mainerprenuer <moustymainer@gmail.com>
2023-07-11 18:41:23 +0100

    created and styled resume Icon with an arrowLink //download

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cceabd6db3b56ffa"; }
+/******/ 		__webpack_require__.h = function() { return "a1f5b7ff595b3d06"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import AnimatedText from '../components/AnimatedText'
 import Head from 'next/head'
 import Image from 'next/image'
 import profilePic from "../../public/images/profile/profilePic.png";
+import Link from 'next/link';
+import { LinkArrow } from '@/components/Icons';
 
 
 export default function Home() {
@@ -21,10 +23,20 @@ export default function Home() {
             </div>
             <div className='w-1/2 flex flex-col items-center self-center'>
               <AnimatedText text="Turning Vision Into Reality With Code And Design." className='!text-6xl !text-left'/>
-            <p>
+            <p className='my-4 text-base font-medium'>
               As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. 
               Explore my latest projects and articles, showcasing my expertise in React.js and web development.
             </p>
+            <div className='flex items-center self-start mt-2'>
+              <Link href="/dummy.pdf" target={'_blank'}
+              className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold
+              hover:bg-light hover:text-dark border-2  border-solid border-transparent hover:border-dark border
+              '
+              // download={true}
+              >Resume <LinkArrow className='w-6 ml-1'/>
+              </Link> 
+              <Link href="mailto:moustymainer@gmail.com" target={'_blank'}>Contact</Link>
+             </div>
             </div>
           </div>
         </Layout>
