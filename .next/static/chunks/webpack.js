7a0aff1da229c1ffcf4511b098163ac70bb34670
mainerprenuer <moustymainer@gmail.com>
2023-07-14 10:45:12 +0100

    skills optimization

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6ced8bf43baa790b"; }
+/******/ 		__webpack_require__.h = function() { return "b357108527abc357"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,9 +8,9 @@ const Skill = ({name, x, y}) => {
         <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light
             p-8 shadow-dark cursor-pointer
             '
-            whileHover={{scale:1.05}}
+            whileHover={{scale:1.09}}
             initialScale={{x:0,y:0}}
-            animate={{x:x, y:y}}
+            whileInView={{x:x, y:y}}
             transition={{duration:1.5}}
             viewport={{once:true}}
             >
@@ -25,33 +25,24 @@ const Skills = () => {
          <h2 className='font-bold text-8xl mt-64 w-full text-center'>Skills</h2>
          <div className='w-full h-screen relative flex items-center justify-center rounded bg-circularLight'>
             <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light
-            px-6 py-3 shadow-dark cursor-pointer absolute
+            p-8 shadow-dark cursor-pointer absolute
             '
             whileHover={{scale:1.05}}>
                 web 
             </motion.div>
 
-            <Skill name="CSS" x="49vw" y="2vw" />
-            <Skill name="HTML" x="60vw" y="-18vw" />
-            <Skill name="Javascript" x="25vw" y="6vw" />
-            <Skill name="Typescript" x="82vw" y="-9vw" />
-            <Skill name="React" x="50vw" y="-12vw" />
+            <Skill name="React" x="50vw" y="-18vw" />
             <Skill name="NextJs" x="4vw" y="-7vw" />
-            <Skill name="NestJs" x="43vw" y="-2vw" />
-            <Skill name="Dart" x="0vw" y="-22vw" />
+            <Skill name="NestJs" x="54vw" y="-2vw" />
+            <Skill name="Mobile Development" x="0vw" y="-22vw" />
             <Skill name="MySql" x="-20vw" y="20vw" />
             <Skill name="Tailwind CSS" x="-4vw" y="25vw" />
-            <Skill name="Figma" x="-25vw" y="15vw" />
+            <Skill name="Figma" x="-33vw" y="5vw" />
             <Skill name="Firebase" x="6vw" y="20vw" />
-            <Skill name="Heroku" x="-10vw" y="-20vw" />
-            <Skill name="Vercel" x="-43vw" y="-1vw" />
-            <Skill name="Git" x="-43vw" y="8vw" />
-            <Skill name="Python" x="-60vw" y="-10vw" />
-            <Skill name="Flutter" x="-85vw" y="-20vw" />
+            <Skill name="Javascript" x="6vw" y="-15vw" />
+            <Skill name="Linux" x="-43vw" y="-10vw" />
             <Skill name="MongoDB" x="-50vw" y="8vw" />
-            <Skill name="Docker" x="-35vw" y="10vw" />
-            <Skill name="Linux" x="-46vw" y="-25vw" /> 
-            <Skill name="PostgreSql" x="-40vw" y="1vw" /> 
+            <Skill name="Web Development" x="-17vw" y="10vw" />
          </div>
     </>
   )
