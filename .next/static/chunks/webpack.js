b38a6983f78d09e07717904ec3bb1e81d52a5dd4
mainerprenuer <moustymainer@gmail.com>
2023-07-15 19:02:36 +0100

    working on projects section.. styling components

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b357108527abc357"; }
+/******/ 		__webpack_require__.h = function() { return "4c0c1624e565bbcd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
