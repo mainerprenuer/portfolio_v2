d8deaa8f029db967ce3ad1197712f6d0563fc316
mainerprenuer <moustymainer@gmail.com>
2023-07-26 23:43:53 +0100

    added responsiveness on the about page mobile view

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3dbf9040994901b4"; }
+/******/ 		__webpack_require__.h = function() { return "5db3de9c8a882e2f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
