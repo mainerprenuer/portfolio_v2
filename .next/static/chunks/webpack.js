a4280bb7b4a2eac55588a77e1f4a5794da226807
mainerprenuer <moustymainer@gmail.com>
2023-07-26 13:37:54 +0100

    added responsiveness to the app layout, padding effects taking changes by screen sizes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4eaee124d22a52fc"; }
+/******/ 		__webpack_require__.h = function() { return "170b119836d06429"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
