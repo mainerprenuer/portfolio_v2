34b4b7403f122b4d82d8ecb17918c54f5719b57c
mainerprenuer <moustymainer@gmail.com>
2023-07-27 17:05:23 +0100

    added responsiveness to the about page and noted some misbehavior on the skills radar component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ab26ac69cf5d564e"; }
+/******/ 		__webpack_require__.h = function() { return "32d46fef32315db1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,7 +7,8 @@ const Skill = ({name, x, y}) => {
     return(
         <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light
             p-8 shadow-dark cursor-pointer dark:text-dark dark:bg-light
-            lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs: bg:transparent xs:dark:bg-transparent
+            lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent xs:dark:bg-transparent
+            xs:text-dark xs:dark:text-light xs:font-bold
             '
             whileHover={{scale:1.09}}
             initialScale={{x:0,y:0}}
@@ -23,7 +24,12 @@ const Skills = () => {
   return (
     <>
          <h2 className='font-bold text-8xl mt-64 w-full text-center md:text-6xl mt:32'>Skills</h2>
-         <div className='w-full h-screen relative flex items-center justify-center rounded-2 bg-circularLight dark:bg-circularDark'>
+         <div className='w-full h-screen relative flex items-center justify-center rounded-2 bg-circularLight dark:bg-circularDark
+         lg:h-[80vh] sm:h-[60vh] xs:h-[50vh]
+         lg:bg-circularLightLg lg:dark:bg-circularDarkLg
+         md:bg-circularLightMd md:dark:bg-circularDarkMd
+         sm:bg-circularLightSm sm:dark:bg-circularDarkSm
+         '>
             <motion.div
              className='flex items-center justify-center rounded-full font-semibold bg-dark text-light
             p-8 shadow-dark cursor-pointer absolute dark:text-dark dark:bg-light
