2029e06258d68efb76b7e39ac38c10f4f4987a4c
mainerprenuer <moustymainer@gmail.com>
2023-08-02 22:19:16 +0100

    rem a eduproject..

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e0ccb853c98be758"; }
+/******/ 		__webpack_require__.h = function() { return "72a45551d4d806e8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
