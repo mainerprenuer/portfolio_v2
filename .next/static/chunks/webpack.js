6a729a64af3443e83da82f584cdb871bc910d62f
mainerprenuer <moustymainer@gmail.com>
2023-08-04 23:12:00 +0100

    update a title..

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "72a45551d4d806e8"; }
+/******/ 		__webpack_require__.h = function() { return "021d1a045eac31b0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
