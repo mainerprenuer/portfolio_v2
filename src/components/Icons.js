76a961835bbf8e992626592986e564268a04afdb
mainerprenuer <moustymainer@gmail.com>
2023-08-17 22:16:50 +0100

    icon upd

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -5,7 +5,7 @@ export const GithubIcon = ({ className, ...rest }) => (
     xmlns="http://www.w3.org/2000/svg"
     width="1em"
     height="1em"
-    viewBox="0 0 512 512"
+    viewBox="0 0 512 512" 
     {...rest}
     className={`w-full h-auto ${className}`}
   >
