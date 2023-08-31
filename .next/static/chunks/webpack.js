8423d60435c28d4616071edd0f5c1050c741ef53
mainerprenuer <moustymainer@gmail.com>
2023-08-31 15:54:12 +0100

    logo update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "edd90aa48c7cba9f"; }
+/******/ 		__webpack_require__.h = function() { return "731266743f4c1397"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
