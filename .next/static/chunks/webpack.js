b84f13445ac815496e5fca89df08bb154f642292
mainerprenuer <moustymainer@gmail.com>
2023-09-01 12:23:12 +0100

    on edu section update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "770e53efddcd032e"; }
+/******/ 		__webpack_require__.h = function() { return "3fc9b02381d3a12a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
